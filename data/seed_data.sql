efed820c3029172945c8259446cf665cd16a7cf8
masatonaka <masatonaka1989@gmail.com>
2020-06-18 22:22:30 +0900

    add seed data

--- data/seed_data.sql
+++ data/seed_data.sql
@@ -7,3 +7,20 @@ VALUES
     ('lax'),
     ('whale'),
     ('ostrich');
+
+
+DELIMITER $$
+CREATE PROCEDURE generate_data()
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    WHILE i < 100000 DO
+    INSERT INTO `from_table` (`name`)
+    VALUES
+        (CONVERT(ROUND(RAND()*100000000,0), CHAR(20)));
+SET i = i + 1;
+END WHILE;
+SELECT count(*) FROM from_table;
+END$$
+DELIMITER ;
+
+CALL generate_data();
